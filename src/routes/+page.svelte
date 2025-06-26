b540c0c7190d69687d1d3cd241cb414a0f1806cd
Lorin Speybrouck <31322750+Daellhin@users.noreply.github.com>
2025-06-26 15:06:00 +0200

    Added Supabase DB

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1 class="text-2xl">Welcome to TRBL</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit" class="link">svelte.dev/docs/kit</a> to read the
+	documentation
+</p>
+
+<a href="/db-testing" class="link">Go to DB Testing</a>
