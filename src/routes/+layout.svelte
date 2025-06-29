c12b7187f332688355d79c6f495a7cdb702f4364
Lorin Speybrouck <31322750+Daellhin@users.noreply.github.com>
2025-06-29 14:03:56 +0200

    Add fontawsome icons and socials

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
-	import '../app.css';
-	
-	let { children } = $props();
+	import Fa from "svelte-fa"
+	import "../app.css"
+
+	import { faDiscord, faFacebookSquare, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons"
+	import { faEnvelope } from "@fortawesome/free-regular-svg-icons"
+
+	let { children } = $props()
 </script>
 
+<div class="m">
+	<!-- Socials -->
+	<div class="flex flex-row gap-2">
+		<a href="https://www.facebook.com/trblsoundandlight" target="_blank">
+			<Fa icon={faFacebookSquare} />
+		</a>
+		<a href="https://www.instagram.com/trbl_snl/" target="_blank">
+			<Fa icon={faInstagram} />
+		</a>
+		<a href="https://www.youtube.com/channel/UCKdrfhbUobqx98V_ViPOHDg" target="_blank">
+			<Fa icon={faYoutube} />
+		</a>
+		<a href="mailto:info@trbl.be" target="_blank">
+			<Fa icon={faEnvelope} />
+		</a>
+		<a href="https://www.discordapp.com/users/_Teunvdv_#5637" target="_blank">
+			<Fa icon={faDiscord} />
+		</a>
+	</div>
+</div>
 {@render children()}
