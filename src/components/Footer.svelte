17ba8b98ac26667acc13ace3e3ae4cfff9fdbc28
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2025-08-27 12:03:38 +0200

    feat: added footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { version } from "$app/environment"
+	
+	let year = new Date().getFullYear()
+</script>
+
+<footer class="footer relative z-20 bg-black flex py-4">
+	<div class="w-full flex justify-around">
+		<div class="text-white text-xs">
+			<span class="whitespace-nowrap">Copyright © {year} TRBL</span> |
+			<span class="whitespace-nowrap">
+				Teun Van de Voorde - Student Zelfstandige
+			</span>
+			|
+			<span class="whitespace-nowrap">BE0791945315</span>
+		</div>
+		<div class="text-white text-xs">
+			<span class="whitespace-nowrap">
+				<a href="/tos" class="link-hover">Algemene Voorwaarden</a></span
+			>
+			|
+			<span class="whitespace-nowrap"
+				><a href="/todo" class="link-hover">Privacyverklaring</a></span
+			>
+			|
+			<a
+				href="https://github.com/TRBLcoding/trbl-website/releases"
+				target="_blank"
+				class="link-hover">Versie: {version}</a
+			>
+		</div>
+	</div>
+</footer>
