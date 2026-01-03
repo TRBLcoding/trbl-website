aea23434c7b772f58b0d3e3d776a41b36b6bb94c
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2026-01-03 22:00:58 +0100

    fix: input component creates keys without values in html output

--- src/components/formHelpers/Input.svelte
+++ src/components/formHelpers/Input.svelte
@@ -30,8 +30,8 @@
 	export let iconRight: IconDefinition | undefined = undefined
 	export let toggleText = type === "password"
 	export let toggled = false
-	export let step = ""
-	export let min = ""
+	export let step: string | undefined = undefined
+	export let min: string | undefined = undefined
 
 	$: inputId =
 		id ||
