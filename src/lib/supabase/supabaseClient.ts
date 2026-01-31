db2f4977bf50787d11f46901958f55bafe2c040b
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2026-01-31 18:00:11 +0100

    fix: "..." is not exported by "\0virtual:env/static/public".

--- src/lib/supabase/supabaseClient.ts
+++ src/lib/supabase/supabaseClient.ts
@@ -1,8 +1,8 @@
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+import { env } from '$env/dynamic/public'
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from './database.types'
 
-export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
+export const supabase = createClient<Database>(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
 
 export const SUPABASE_STORAGE_URL = "https://teiboeelbjhxmsiyzpsn.supabase.co/storage/v1/object/public/"
 
@@ -12,8 +12,8 @@ export function createSupabaseStorageUrl(bucket: string, folder: string, fileNam
 
 export function getSupabaseClientFromToken(jwt: string) {
 	return createClient<Database>(
-		PUBLIC_SUPABASE_URL,
-		PUBLIC_SUPABASE_ANON_KEY,
+		 env.PUBLIC_SUPABASE_URL,
+		 env.PUBLIC_SUPABASE_ANON_KEY,
 		{
 			global: {
 				headers: {
