4dbc584e5a9021ceb3f71aaad3f47c0ede686d4b
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2026-05-17 18:17:43 +0200

    chore: use env variable for SUPABASE_STORAGE_URL

--- src/lib/supabase/supabaseClient.ts
+++ src/lib/supabase/supabaseClient.ts
@@ -2,12 +2,11 @@ import { env } from '$env/dynamic/public'
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from './database.types'
 
-const SUPABASE_URL = env.PUBLIC_SUPABASE_URL || 'https://placeholder.supabase.co'
-const SUPABASE_ANON_KEY = env.PUBLIC_SUPABASE_ANON_KEY || 'placeholder-key'
+const SUPABASE_URL = env.PUBLIC_SUPABASE_URL
+const SUPABASE_ANON_KEY = env.PUBLIC_SUPABASE_ANON_KEY
+const SUPABASE_STORAGE_URL =env.PUBLIC_SUPABASE_ANON_KEY + "/storage/v1/object/public/"
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY)
-
-export const SUPABASE_STORAGE_URL = "https://teiboeelbjhxmsiyzpsn.supabase.co/storage/v1/object/public/"
 
 export function createSupabaseStorageUrl(bucket: string, folder: string, fileName: string) {
 	return `${SUPABASE_STORAGE_URL}${bucket}/${folder}${fileName}.webp`
